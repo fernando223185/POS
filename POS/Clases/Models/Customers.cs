91402c35122548bec87bcc98c95b02525f85c44c
fernando223185 <fernando.hdez.marq.22@gmail.com>
2024-03-11 18:17:28 -0600

    Customer_Module

--- POS/Clases/Models/Customers.cs
+++ POS/Clases/Models/Customers.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RestSharp;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,37 @@ namespace POS.Clases.Models
 {
     public class Customers
     {
+        private string urlApi = "https://localhost:7254/api/Customer/";
 
+        public async Task<RestResponse> GetCustomers()
+        {
+            var customer = new RestClient(urlApi+"List");
+            var request = new RestRequest();
+            request.Method = Method.Post;
+            var parametros = new
+            {
+                page = 1,
+                search = ""
+            };
+
+            request.AddJsonBody(parametros);
+
+            try
+            {
+
+                var response = await customer.ExecuteAsync(request);
+
+                return (RestResponse)response;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+        public Customers()
+        {
+
+        }
     }
 }
--- POS/Views/Customers/Customer.cs
+++ POS/Views/Customers/Customer.cs
@@ -1,30 +1,36 @@
 ﻿using System;
 using System.Windows.Forms;
+using POS.Clases;
+using POS.Clases.Models;
 
 namespace POS.Views.Customers
 {
     public partial class Customer : Form
     {
+        Clases.Models.Customers customer = new Clases.Models.Customers();
         public Customer()
         {
             InitializeComponent();
+            getCustomers();
+
         }
 
-        private void Customer_Load(object sender, EventArgs e)
+/*        private void Customer_Load(object sender, EventArgs e)
         {
             getCustomers();
-        }
+        } */
 
-        private void getCustomers()
+        private async void getCustomers()
         {
 
             dataCte.Rows.Clear();
-            var customer = await login.GetLogin(
+            var customers = await customer.GetCustomers();
+            MessageBox.Show(customers.Content);
 
-            foreach (var customer in ConsultarProductos())
+            /*foreach (var customer in ConsultarProductos())
             {
                 dataCte.Rows.Add(producto.Id, producto.Nombre, producto.Precio);
-            }
+            } */
 
         }
     }
